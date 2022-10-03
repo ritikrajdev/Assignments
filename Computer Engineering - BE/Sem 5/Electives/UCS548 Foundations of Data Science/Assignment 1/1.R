834d35dab1eccef1d184237a6028e3983fd20fe0
Khushi Prasad <khushi@Khushis-MacBook-Air-3.local>
2022-10-04 00:18:08 +0530

    Data Science Assignments

---
+++ Computer Engineering - BE/Sem 5/Electives/UCS548 Foundations of Data Science/Assignment 1/1.R
@@ -0,0 +1,49 @@
+# Question 1
+my_list<-list(A=23.4,B=45,C=678)
+print(my_list)
+
+# Question 2
+print(my_list)
+
+# Question 3
+my_list[-3]
+
+# Question 4
+# I am learning R
+
+# Question 5
+MyName<-"Khushi"
+MySurname<-"Prasad"
+print(paste(MyName,MySurname))
+
+# Question 6
+n=FALSE
+n=ifelse(n=="TRUE",1,0)
+n
+
+# Question 7
+A <- 23.4
+B <- 45
+C <- 678
+print(A+B+C)
+print(A-B-C)
+print(A*B*C)
+print(A/B/C)
+
+# Question 8
+exp(1)
+log(10)
+log10(1000)
+log2(32)
+pi
+sqrt(121)
+
+# Question 9
+# 1
+23+(4.5*2.3)/10
+# 2
+456/12-log(90)
+# 3
+exp(5)+12/(5^6)
+# 4
+sqrt(45)*12/3
---
+++ Computer Engineering - BE/Sem 5/Electives/UCS548 Foundations of Data Science/Assignment-2.1/3.R
@@ -0,0 +1,42 @@
+# Question 1
+A<-c(23,34,56,3,0,0)
+B<-c("c","b","a")
+print(A)
+print(B)
+
+# Question 2
+n=as.integer(readline("Enter the value of n:"))
+m=as.integer(readline("Enter the value of m:"))
+print(seq(from=1, to=n, by=0.6))
+print(seq(from=m, to=n, by=0.6))
+
+# Question 3
+x<-1:50
+pretty(x, n=10)
+
+# Question 4
+for (i in 1:5)
+{
+  print("3,4,5")
+}
+
+# Question 5
+p<-rnorm(8)
+print(p)
+
+# Question 6
+C<-c(2,3,4,5,6,7)
+print(A+C)
+
+# Question 7
+D<-c(6,7)
+print(A+D)
+
+# Question 8
+o<-sqrt(C)
+print(o)
+
+# Question 9
+length(A)
+length(B)
+length(C)
